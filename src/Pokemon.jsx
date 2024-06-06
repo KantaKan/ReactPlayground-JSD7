11d210394b35dbfb73c347f8d394a2b95da826f3
fiadaze <fiakantapon@gmail.com>
2024-06-06 13:25:29 +0700

    fix some code

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -5,7 +5,28 @@ const Pokemon = () => {
     const [ranNum , setRanNum] = useState(0)
     const [randomPokemon, setRandomPokemon] = useState(null);
     const [loading, setLoading] = useState(true)
+    const [detailsLoading, setDetailsLoading] = useState(false)
     const pokeApi = "https://pokeapi.co/api/v2/pokemon?limit=200"
+    const typeColors = {
+        fire: "bg-red-500",
+        water: "bg-blue-500",
+        grass: "bg-green-500",
+        electric: "bg-yellow-500",
+        psychic: "bg-pink-500",
+        ice: "bg-blue-200",
+        dragon: "bg-purple-500",
+        dark: "bg-gray-800",
+        fairy: "bg-pink-300",
+        normal: "bg-gray-400",
+        fighting: "bg-orange-700",
+        flying: "bg-indigo-300",
+        poison: "bg-purple-700",
+        ground: "bg-yellow-700",
+        rock: "bg-yellow-900",
+        bug: "bg-green-700",
+        ghost: "bg-purple-900",
+        steel: "bg-gray-500",
+      };
     useEffect(() => {
         const fetchpokemon = async () => {
              try {
@@ -27,6 +48,7 @@ const Pokemon = () => {
                 const response = await fetch(pokemon[ranNum].url)
                 const data = await response.json()
                 setRandomPokemon(data)
+                setDetailsLoading(false)
             } catch (error) {
                 console.log(error)
             }
@@ -58,7 +80,7 @@ const Pokemon = () => {
                             </h3>
                             <p>Types : {randomPokemon.types.map((type) => type.type.name).join(', ')}</p>
                             <div className="card-actions">
-                                <button onClick={handleRandomPoke} className="btn btn-primary">Random Pokemon !!</button>
+                                <button onClick={handleRandomPoke} className="btn btn-primary " disabled={detailsLoading} >{detailsLoading ? <span  className="loading loading-spinner"></span> : "Random Pokemon !!"} </button>
                             </div>
                         </div>
                     </>
